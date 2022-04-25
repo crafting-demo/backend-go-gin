1b62e29f19e8d1d4586519b7e65a2529a94747d0
Alexander Maru <i@alexandermaru.com>
2022-04-25 01:57:36 +0000

    update kafka clients

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -2,32 +2,44 @@ package api
 
 import (
 	"encoding/json"
+	"log"
 	"os"
 	"os/signal"
 
+	"github.com/Shopify/sarama"
 	"github.com/crafting-demo/backend-go/pkg/kafka"
 )
 
 func Run() {
-	signalCh := make(chan os.Signal, 1)
-	signal.Notify(signalCh, os.Interrupt)
-
 	consumer := kafka.Consumer{Topic: Go}
 
-	messages, err := consumer.Messages()
+	conn, err := consumer.New()
 	if err != nil {
+		log.Println("Failed to create new consumer", err)
 		return
 	}
+	defer conn.Close()
+
+	partitionConsumer, err := conn.ConsumePartition(consumer.Topic, 0, sarama.OffsetOldest)
+	if err != nil {
+		log.Println("Failed to create partition consumer", err)
+	}
+	defer partitionConsumer.Close()
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
 
 	for {
 		select {
-		case <-signalCh:
-			return
-		case msg := <-messages:
+		case msg := <-partitionConsumer.Messages():
 			var message kafka.Message
-			if err := json.Unmarshal(msg.Value, &message); err == nil {
-				Process(message)
+			if err := json.Unmarshal(msg.Value, &message); err != nil {
+				log.Println("Failed to parse json encoded message", err)
+				continue
 			}
+			go Process(message)
+		case <-signals:
+			return
 		}
 	}
 }
